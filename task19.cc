280c5c37e77ccde1397fd6edfcd9f5cde30cbe36
Nikita Vasetskiy <tv42.vasetskiy.nikita@gmail.com>
2025-05-02 21:58:39 +0300

    Додав зміну яка бууде зхберігати унікальний номер для кожного доміно,та массив для цих унікальних номерів який буде зберігати розташування цих доміно

--- task19.cc
+++ task19.cc
@@ -11,20 +11,24 @@ int array[ROWS][COLS] = {
         {1,4,1,4,5,6,6,5},
         {6,6,3,0,3,3,6,2}
 };
-int usedDomino[ROWS][COLS] = {};
+bool usedDomino[ROWS][COLS] = {};
+int dominoId = 1;
+int dominoMap[ROWS][COLS] = {};
 int isValid(int r, int c) {
     return r >= 0 && r < ROWS && c >= 0 && c < COLS && !usedDomino[r][c];
 }
-int Domino(int row,int col,int array[ROWS][COLS]){
-    if(row==ROWS){
+int Domino(int r,int c){
+    if(r==ROWS){
         return 1;
     }
-    if (col==COLS){
-        return Domino(row + 1, 0,array);
+    if (c==COLS){
+        return Domino(r + 1, 0);
     }
-    cout << "Проходимо комірку (" << row << ", " << col << ")зі значенням: " 
-         << array[row][col] << endl;
-    return Domino(row, col + 1,array);
+    if (usedDomino[r][c]) return Domino(r, c + 1);
+    int a = array[r][c];
+    usedDomino[r][c] = true;
+    cout<<"Комірка ["<<r<<"]["<<c<<"]="<<a<<endl;
+    return Domino(r, c + 1);
 }
 int main(){
     
@@ -38,6 +42,6 @@ int main(){
         cout << "|" << endl; 
     }
     cout << "+------------------------+" << endl;
-    Domino(0,0,array);
+    Domino(0,0);
     return 0;
 }
