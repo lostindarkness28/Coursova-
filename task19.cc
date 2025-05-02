3b2d0bef1cc5113003e771564681465fb019e50a
Nikita Vasetskiy <tv42.vasetskiy.nikita@gmail.com>
2025-05-03 00:16:10 +0300

    Додав функцію яка виписує всі знайдені доміно попорядку

--- task19.cc
+++ task19.cc
@@ -1,52 +1,53 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 const int ROWS = 7;
 const int COLS = 8;
+
 int array[ROWS][COLS] = {
-        {4,6,2,5,5,2,0,1},
-        {0,4,4,0,0,1,6,3},
-        {2,4,4,1,1,3,1,5},
-        {2,5,0,2,2,2,0,0},
-        {5,3,3,6,4,1,3,5},
-        {1,4,1,4,5,6,6,5},
-        {6,6,3,0,3,3,6,2}
+    {4,6,2,5,5,2,0,1},
+    {0,4,4,0,0,1,6,3},
+    {2,4,4,1,1,3,1,5},
+    {2,5,0,2,2,2,0,0},
+    {5,3,3,6,4,1,3,5},
+    {1,4,1,4,5,6,6,5},
+    {6,6,3,0,3,3,6,2}
 };
+
 bool usedCell[ROWS][COLS] = {};
-bool usedDomino[7][7]={};
+bool usedDomino[7][7] = {};
 int dominoId = 1;
 int dominoMap[ROWS][COLS] = {};
+
 int isValid(int r, int c) {
     return r >= 0 && r < ROWS && c >= 0 && c < COLS && !usedCell[r][c];
 }
-int Domino(int r,int c){
-    if(r==ROWS){
-        return 1;
-    }
-    if (c==COLS){
-        return Domino(r + 1, 0);
-    }
+bool Domino(int r, int c) {
+    if (r == ROWS) return true;
+    if (c == COLS) return Domino(r + 1, 0);
     if (usedCell[r][c]) return Domino(r, c + 1);
     int a = array[r][c];
     usedCell[r][c] = true;
-    if (isValid(r, c + 1)){
-         int b = array[r][c + 1];
-         int x = min(a, b), y = max(a, b); 
-         if (!usedDomino[x][y]){
+    if (isValid(r, c + 1)) {
+        int b = array[r][c + 1];
+        int x = min(a, b), y = max(a, b);
+        if (!usedDomino[x][y]) {
             usedDomino[x][y] = true;
-            usedCell[r][c+1] = true;
+            usedCell[r][c + 1] = true;
             dominoMap[r][c] = dominoId;
-            dominoMap[r][c+1] = dominoId;
+            dominoMap[r][c + 1] = dominoId;
             dominoId++;
             if (Domino(r, c + 2)) return true;
             dominoId--;
             usedDomino[x][y] = false;
             usedCell[r][c + 1] = false;
-         }
+        }
     }
-    if (isValid(r + 1, c)){
-        int b = array[r+1][c];
-        int x = min(a, b), y = max(a, b); 
-         if (!usedDomino[x][y]){
+    if (isValid(r + 1, c)) {
+        int b = array[r + 1][c];
+        int x = min(a, b), y = max(a, b);
+        if (!usedDomino[x][y]) {
             usedDomino[x][y] = true;
             usedCell[r + 1][c] = true;
             dominoMap[r][c] = dominoId;
@@ -55,10 +56,39 @@ int Domino(int r,int c){
             if (Domino(r, c + 1)) return true;
             dominoId--;
             usedDomino[x][y] = false;
-            usedCell[ r+1 ][c] = false;
+            usedCell[r + 1][c] = false;
         }
     }
-    return 0;
+
+    usedCell[r][c] = false;
+    return false;
+}
+void printDomino() {
+    cout << "\nСписок доміно:\n\n";
+    struct Domino {
+        int a = -1;
+        int b = -1;
+    };
+    vector<Domino> dominos(dominoId); 
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            int id = dominoMap[i][j];
+            if (id > 0) {
+                if (dominos[id].a == -1)
+                    dominos[id].a = array[i][j];
+                else
+                    dominos[id].b = array[i][j];
+            }
+        }
+    }
+    int count = 0;  
+    cout<<"---------  ---------  ---------  ---------  ---------  ---------  ---------"<<endl;
+    for (int i = 1; i < dominoId; i++) {
+        cout << "| " << dominos[i].a << " | " << dominos[i].b << " |  ";
+        count++;
+        if (count % 7 == 0) cout<<endl<<"---------  ---------  ---------  ---------  ---------  ---------  ---------"<< endl;
+    }
+    cout << endl;
 }
 int main(){
     
@@ -73,5 +103,6 @@ int main(){
     }
     cout << "+------------------------+" << endl;
     Domino(0,0);
+    printDomino();
     return 0;
 }
