65cf43af08c41cb4b26795c6c432c49e4ff22e8f
Nikita Vasetskiy <tv42.vasetskiy.nikita@gmail.com>
2025-05-05 19:46:21 +0300

    Додано обмеження на використання цифр в певних рядках і стовпчиках

--- Ex2.cc
+++ Ex2.cc
@@ -15,6 +15,21 @@ bool avCells[ROWS][COLS] = {
     { true,  false, false, false, true,  false, true,  true,  false, false, true },
     { true,  true,  true,  true,  true,  false, false, true,  true,  true,  true }
 };
+bool rowDigits[ROWS][7] = {}; 
+bool colDigits[COLS][7] = {};
+void RowTrue() {
+    rowDigits[0][2] = true; rowDigits[0][0] = true; rowDigits[0][1] = true; rowDigits[0][5] = true;
+    rowDigits[6][1] = true; rowDigits[6][3] = true; rowDigits[6][4] = true;
+    rowDigits[9][0] = true; rowDigits[9][2] = true; rowDigits[9][3] = true;
+}
+void ColTrue() {
+    colDigits[0][2] = true; colDigits[0][4] = true; colDigits[0][6] = true;
+    colDigits[2][0] = true; colDigits[2][1] = true; colDigits[2][5] = true;
+    colDigits[4][0] = true; colDigits[4][3] = true; colDigits[4][6] = true;
+    colDigits[6][1] = true; colDigits[6][3] = true; colDigits[6][6] = true;
+    colDigits[8][0] = true; colDigits[8][1] = true; colDigits[8][5] = true;
+    colDigits[10][1] = true; colDigits[10][2] = true; colDigits[10][3] = true; colDigits[10][4] = true;
+}
 int main(){
     cout<<"     Початкове поле          "<< endl;
     cout << "+--------+            +--------+" << endl;
