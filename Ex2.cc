85c75dca01393382b61e1dab61bdf47e35b24e23
Nikita Vasetskiy <tv42.vasetskiy.nikita@gmail.com>
2025-05-08 14:16:08 +0300

    Додано зміну яка перевіряє чи є обмеження в стовбця або рядка на використання цифр

--- Ex2.cc
+++ Ex2.cc
@@ -17,17 +17,26 @@ bool avCells[ROWS][COLS] = {
 };
 bool rowDigits[ROWS][7] = {}; 
 bool colDigits[COLS][7] = {};
-void RowTrue() {
+bool specificRow[ROWS]={};
+bool specificCol[COLS]={};
+void specificValues() {
+    specificRow[0]=true;
     rowDigits[0][2] = true; rowDigits[0][0] = true; rowDigits[0][1] = true; rowDigits[0][5] = true;
+    specificRow[6]=true;
     rowDigits[6][1] = true; rowDigits[6][3] = true; rowDigits[6][4] = true;
+    specificRow[9]=true;
     rowDigits[9][0] = true; rowDigits[9][2] = true; rowDigits[9][3] = true;
-}
-void ColTrue() {
+    specificCol[0]=true;
     colDigits[0][2] = true; colDigits[0][4] = true; colDigits[0][6] = true;
+    specificCol[2]=true;
     colDigits[2][0] = true; colDigits[2][1] = true; colDigits[2][5] = true;
+    specificCol[4]=true;
     colDigits[4][0] = true; colDigits[4][3] = true; colDigits[4][6] = true;
+    specificCol[6]=true;
     colDigits[6][1] = true; colDigits[6][3] = true; colDigits[6][6] = true;
+    specificCol[8]=true;
     colDigits[8][0] = true; colDigits[8][1] = true; colDigits[8][5] = true;
+    specificCol[10]=true;
     colDigits[10][1] = true; colDigits[10][2] = true; colDigits[10][3] = true; colDigits[10][4] = true;
 }
 int dominoId = 1;
